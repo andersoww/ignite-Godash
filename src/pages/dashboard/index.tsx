f813e81b1828224c73ae838e7855cdb8cb7151b2
Anderson Roque <andersonbarrinha18@gmail.com>
2022-02-15 18:16:16 -0300

    Dashboard

--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -1,8 +1,58 @@
-import { Box, Flex, SimpleGrid, Text } from "@chakra-ui/react";
+import { Box, Flex, SimpleGrid, Text, theme } from "@chakra-ui/react";
 import { Header } from "../../components/Header";
 import SideBar from "../../components/SideBar";
-import Chart from "react-apexcharts";
-const options = {};
+import dynamic from "next/dynamic";
+import { ApexOptions } from "apexcharts";
+const Chart = dynamic(() => import("react-apexcharts"), {
+  ssr: false,
+});
+const options: ApexOptions = {
+  chart: {
+    toolbar: {
+      show: false,
+    },
+    zoom: {
+      enabled: false,
+    },
+    foreColor: theme.colors.gray[500],
+  },
+  grid: {
+    show: false,
+  },
+  dataLabels: {
+    enabled: false,
+  },
+  tooltip: {
+    enabled: false,
+  },
+  xaxis: {
+    type: "datetime",
+    axisBorder: {
+      color: theme.colors.gray[600],
+    },
+    axisTicks: {
+      color: theme.colors.gray[600],
+    },
+    categories: [
+      "2022-02-18T00:00:00.000Z",
+      "2022-02-19T00:00:00.000Z",
+      "2022-02-20T00:00:00.000Z",
+      "2022-02-21T00:00:00.000Z",
+      "2022-02-22T00:00:00.000Z",
+      "2022-02-23T00:00:00.000Z",
+      "2022-02-24T00:00:00.000Z",
+    ],
+  },
+  fill: {
+    opacity: 0.3,
+    type: "gradient",
+    gradient: {
+      shade: "dark",
+      opacityFrom: 0.7,
+      opacityTo: 0.3,
+    },
+  },
+};
 
 const series = [{ name: "series1", data: [31, 120, 10, 28, 61, 18, 109] }];
 export default function Dashboard() {
@@ -13,10 +63,13 @@ export default function Dashboard() {
       <Flex w="100%" my="6" maxWidth={1480} mx="auto" px="6">
         <SideBar />
         <SimpleGrid flex="1" gap="4" minChildWidth="320px">
-          <Box p="8" bg="gray.800" borderRadius={8}>
-            <Text>Inscritos da Semana</Text>
+          <Box p="8" bg="gray.800" borderRadius={8} pb="4">
+            <Text fontSize="lg" mb="4">
+              Inscritos da Semana
+            </Text>
+            <Chart options={options} series={series} type="area" height={160} />
           </Box>
-          <Box p="8" bg="gray.800" borderRadius={8}>
+          <Box p="8" bg="gray.800" borderRadius={8} pb="4">
             <Text fontSize="lg" mb="4">
               Taxa de Abertura
             </Text>
